566c7bd7629db8f54a78649b2318854aaea93d42
Adan Yao <adanyao@gmail.com>
2023-04-01 01:34:14 +0800

    update04010134

---
+++ tests/test_addition.py
@@ -0,0 +1,6 @@
+def test_addition():
+    assert 1 + 2 == 5 
+
+def test_addition_with_negative_numbers():
+    assert -1 + 2 == 1
+
---
+++ tests/test_subtraction.py
@@ -0,0 +1,6 @@
+def test_subtraction():
+    assert 5 - 3 == 2
+
+def test_subtraction_with_negative_numbers():
+    assert -5 - (-3) == -2
+
